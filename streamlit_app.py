2003dc5bff4ae53f37174357d41cea193e4d8781
Shaif Chowdhury <shaif_chowdhury1@baylor.edu>
2023-08-10 09:52:11 -0500

    Added

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,18 +16,14 @@ st.write(df.head(2))
 
 #Lucy 
 #Does smoking increase the number of awakenings during the night? Histogram of Awakenings with Smoking as Hue
-
 st.header("Does smoking decrease Sleep Efficiency during the night?")
-
 cross_tab_prop = pd.crosstab(index=df['Sleep efficiency'],
                              columns=df['Smoking status'],
                              normalize="index")
-
 # Create a Streamlit app
 st.title("Sleep Efficiency vs. Smoking Status")
 st.write("Cross Tabulation:")
 #st.write(cross_tab_prop.head())
-
 # Create the bar plot
 st.write("Bar Plot:")
 fig, ax = plt.subplots(figsize=(10, 6))
@@ -36,8 +32,9 @@ plt.legend(loc="upper left", ncol=2)
 plt.xlabel("Sleep efficiency")
 plt.ylabel("Proportion")
 st.pyplot(fig)
+st.text("We wanted to see whether smoking would have an effect on people's sleep quality. Our data showed that the proportion of people who smoked was higher in people who had worse sleep quality, likely since the substances found in cigarettes like nicotine can disrupt sleep and act as a stimulant.")
 
-
+#Does age impact one's quality of sleep? Scatterplot
 # Define age groups and labels
 Age_Group = [(9, 21, 'Young'), (22, 34, 'Younger Adult'), (35, 47, 'Middle Aged'), (48, 69, 'Older')]
 # Create Age-Group column based on Age
@@ -48,14 +45,11 @@ def assign_age_group(age):
     return 'Unknown'
 
 df['Age-Group'] = df['Age'].apply(assign_age_group)
-
-st.text("We also wanted to see whether smoking would have an effect on people's sleep quality. Our data showed that the proportion of people who smoked was higher in people who had worse sleep quality, likely since the substances found in cigarettes like nicotine can disrupt sleep and act as a stimulant.")
-#Does age impact one's quality of sleep? Scatterplot
 st.header("Does one's age have an impact on their sleep efficiency?")
 sns.lineplot(
   data = df, 
   x = "Age-Group", 
-  y = "Sleep efficiency",)
+  y = "Sleep efficiency")
 
 #Do people who go to bed later or earlier have a better quality of sleep? Scatterplot
 
@@ -70,7 +64,7 @@ sns.lineplot(
 #How caffeine consumption relates to sleep efficiency? Scatterplot
 st.header("How caffeine consumption relates to sleep efficiency? ")
 
-fig = px.scatter(df, x = "Sleep efficiency", y = "Caffeine consumption", color = "Gender", size = "Sleep efficiency")
+fig = px.scatter(df, x = "Sleep efficiency", y = "Caffeine consumption", color = "Gender")
 
 st.plotly_chart(fig)
 
@@ -79,7 +73,9 @@ st.text("There doesn't seem to be much correlation between a person's caffeine c
 #Do people who get less REM sleep tend to drink more caffeine? Scatterplot
 st.header("Do people who get less REM sleep tend to drink more caffeine?")
 
-px.scatter(df, x = "Caffeine consumption", y = "REM sleep percentage", color = "Gender", size = "REM sleep percentage")
+fig = px.scatter(df, x = "Caffeine consumption", y = "REM sleep percentage", color = "Gender")
+
+st.plotly_chart(fig)
 
 st.text("The data shows that people's caffeine consumption isn't necessarily effected by their REM sleep percentage and vice versa. There seems to be a steady number of people getting the same amount of REM sleep no matter their caffeine consumption.")
 
